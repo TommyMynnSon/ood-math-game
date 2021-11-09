fe9ff10ae90a225e9ea14d5df7c1bc24433be05e
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-11-09 02:52:21 -0800

    Add Life class with initial set up

---
+++ life.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# Life class
+class Life
+  STARTING_LIVES = 3
+
+  @@id = 0
+
+  attr_accessor :id, :lives
+
+  def initialize
+    @@id += 1
+
+    self.id = @@id
+    self.lives = STARTING_LIVES
+  end
+
+  def decrease
+    self.lives -= 1
+  end
+
+  def to_s
+    "Life { id: #{id}, lives: #{lives} }"
+  end
+end
