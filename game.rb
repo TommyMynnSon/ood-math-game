4d8b6e2d585d23333c62596f2162ecc7f2cd2823
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-11-09 07:13:11 -0800

    Add Game class

---
+++ game.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require_relative 'question'
+
+# Game class
+class Game
+  @@id = 0
+
+  attr_accessor :id, :players
+
+  def initialize(players)
+    @@id += 1
+
+    self.id = @@id
+    self.players = players
+  end
+
+  def one_remaining?
+    not_out_count = 0
+
+    players.each do |player|
+      not_out_count += 1 unless player.out?
+    end
+
+    not_out_count == 1
+  end
+
+  def tie?
+    out_count = 0
+
+    players.each do |player|
+      out_count += 1 if player.out?
+    end
+
+    out_count == players.length
+  end
+
+  def start
+    while !one_remaining? && !tie?
+      turn = Turn.new(players)
+      turn.ask
+      GameAnnouncer.announce_score(players)
+    end
+
+    one_remaining? ? GameAnnouncer.announce_winner(players) : GameAnnouncer.announce_tie
+
+    GameAnnouncer.announce_game_over
+  end
+end
